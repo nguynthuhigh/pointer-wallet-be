75dcecd56b603feba7e3acf4f58ffd8d61a416c4
Nguyễn Minh Nguyên <87266672+nguynthuhigh@users.noreply.github.com>
2024-07-04 03:09:18 +0700

    Update role.middleware.js

--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -26,28 +26,32 @@ exports.verifyRole = (role)=>{
 }
 exports.Authenciation =(role)=>{
     return async (req,res,next)=>{
-
-        const token = req.headers.authorization?.split(' ')[1];
-        if(token){
-            const result = await tokenAuth.verifyToken(token);
-            if(role == ROLE.USER){
-                const user = await User.findById(result.id);
-                if(user){
-                    req.user = result.id
-                    req.security_code = user.security_code
-                    next()
+        try{
+              const token = req.headers.authorization?.split(' ')[1];
+                if(token){
+                    const result = await tokenAuth.verifyToken(token);
+                    if(role == ROLE.USER){
+                        const user = await User.findById(result.id);
+                        if(user){
+                            req.user = result.id
+                            req.security_code = user.security_code
+                            next()
+                        }
+                    }
+                    if(role == ROLE.PARTNER){
+                        const partner = await Partner.findById(result.id);
+                        if(partner){
+                            req.partner = result.id
+                            next()
+                        }
+                    }
                 }
-            }
-            if(role == ROLE.PARTNER){
-                const partner = await Partner.findById(result.id);
-                if(partner){
-                    req.partner = result.id
-                    next()
+                else{
+                    return res.status(404).json({message:"Page not found"})
                 }
-            }
-        }
-        else{
+        }catch(error){
             return res.status(404).json({message:"Page not found"})
         }
+      
     }
 }
