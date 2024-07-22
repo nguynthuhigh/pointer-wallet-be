ee6aedb066aa59332f91a5a841985d16438bdbf2
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-07-22 23:43:41 +0700

    feat: qrcode

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary')
 
 cloudinary.config({ 
-    cloud_name: 'ds0cz9t6b', 
-    api_key: '343174643682814', 
-    api_secret: process.env.CLOUD_API_SECRET // Click 'View Credentials' below to copy your API secret
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_API_KEY, 
+    api_secret: process.env.CLOUD_API_SECRET 
 });
 
 module.exports = cloudinary
