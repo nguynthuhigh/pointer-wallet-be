9e587b48f798ae78dced6a0dcd7e4edb5131b9e1
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-07-30 16:02:49 +0700

    voucher

--- services/voucher.servcies.js
+++ services/voucher.servcies.js
@@ -30,7 +30,7 @@ module.exports ={
         if (type === "discount_amount") {
             result = amount - discountValue;
             if(result < 0){
-                result = 0
+                return result = 0
             }
         } else if (type === "discount_percent") {
             result = amount - (amount * (discountValue / 100));
