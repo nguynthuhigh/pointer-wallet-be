0ac9c32b499d69e3d50b7e401e3384481ea320cb
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-07-30 13:40:34 +0700

    fix

--- utils/convert_currency.js
+++ utils/convert_currency.js
@@ -1,9 +1,5 @@
-const convertCurrency = (amount, currency) => {
+exports.convertCurrency = (amount, currency) => {
     if (currency === "VND") return amount;
     if (currency === "USD") return (amount * 25200);
     if (currency === "ETH") return (amount * 25000 * 3500);
   };
-  const convert = {
-    convertCurrency,
-  };
-module.exports =  convert;
