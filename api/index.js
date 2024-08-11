ee3938939fea73f67fb03efc8bb11f5c5b6a1286
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-08-11 17:29:00 +0700

    deploy: vercel with dir api

---
+++ api/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+require('../cron')
+const express = require("express");
+const app = express();
+const {connectMongoDB} = require('../configs/mongodb/mongodb')
+const {connectRedis} = require('../configs/redis/redis')
+//Config cors
+const cors = require("cors");
+app.use(cors());
+//bodyParser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Routes
+require('../routes/index')(app)
+//Connect to MongoDB
+connectMongoDB()
+//Connect to Redis
+connectRedis()
+
+app.listen({ port: process.env.PORT }, () => {
+  console.log("http://localhost:" + process.env.PORT);
+});
+module.exports = app
