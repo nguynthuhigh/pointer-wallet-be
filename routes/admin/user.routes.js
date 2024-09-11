dacfb0cc4f5ac3c9a5e208d6fcf6c1964c5c9683
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-09-11 22:05:59 +0700

    clear auth

--- routes/admin/user.routes.js
+++ routes/admin/user.routes.js
@@ -5,7 +5,7 @@ const ROLE = require('../../contains/role')
 const controller = require('../../controllers/admin/user_management.controller')
 const upload = require('../../middlewares/multer.middleware')
 const {authenticationUser} = require('../../middlewares/authentication.middleware')
-router.get('/get-users',roleAuth.Authentication_Admin(ROLE.ADMIN),controller.getUsers)
+router.get('/get-users',controller.getUsers)
 router.put('/ban-user',roleAuth.Authentication_Admin(ROLE.ADMIN),controller.banUser)
 router.get('/get-transactions',roleAuth.Authentication_Admin(ROLE.ADMIN),controller.getTransactionsUser)
 
