1d2cc713d87b45ecba79fbc0dbc641beafcf133b
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-09-26 14:53:01 +0700

    deploy vercel

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,40 @@
 require("dotenv").config();
-require('../cron')
+require("../cron");
 const express = require("express");
 const app = express();
-const {connectMongoDB} = require('../configs/mongodb/mongodb')
-const {connectRedis} = require('../configs/redis/redis')
+const { connectMongoDB } = require("../configs/mongodb/mongodb");
+const { connectRedis } = require("../configs/redis/redis");
 const handleErrorMiddleware = require("../middlewares/handleError.middleware");
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 //Config cors
 const cors = require("cors");
-app.use(cors({
-  origin:['http://localhost:5173','https://presspay-wallet.vercel.app','http://localhost:3000','https://presspay.vercel.app'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://192.168.1.218:3000",
+      "https://pointer.io.vn",
+      "https://wallet.pointer.io.vn",
+      "http://localhost:5173",
+      "https://presspay-wallet.vercel.app",
+      "http://localhost:3000",
+      "https://presspay.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 //bodyParser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Routes
-require('../routes/index')(app)
+require("../routes/index")(app);
 //Connect to MongoDB
-connectMongoDB()
+connectMongoDB();
 //Connect to Redis
-connectRedis()
-app.use(handleErrorMiddleware)
+connectRedis();
+app.use(handleErrorMiddleware);
 app.listen({ port: process.env.PORT }, () => {
   console.log("http://localhost:" + process.env.PORT);
 });
-module.exports = app
+module.exports = app;
